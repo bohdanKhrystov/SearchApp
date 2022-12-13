6f01d990d295de87bd36dd01565aaaf860b316da
Bohdan Khrystov <bogdankhrystov@gmail.com>
2022-12-13 19:23:04 +0200

    refactor: cleanup

--- app/src/main/java/com/bohdanhub/searchapp/ui/feature/home/Home.kt
+++ app/src/main/java/com/bohdanhub/searchapp/ui/feature/home/Home.kt
@@ -1,12 +1,9 @@
 package com.bohdanhub.searchapp.ui.feature.home
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
