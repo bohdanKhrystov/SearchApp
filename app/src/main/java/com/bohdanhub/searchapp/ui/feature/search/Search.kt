228799c456c014bfb01c2c2f27cd129ae0ec1410
Bohdan Khrystov <bogdankhrystov@gmail.com>
2022-12-14 16:18:22 +0200

    feat: search bottom sheet basics

--- app/src/main/java/com/bohdanhub/searchapp/ui/feature/search/Search.kt
+++ app/src/main/java/com/bohdanhub/searchapp/ui/feature/search/Search.kt
@@ -1,33 +1,70 @@
 package com.bohdanhub.searchapp.ui.feature.search
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SearchScreen() {
+    val sheetState: ModalBottomSheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+    )
+    val coroutineScope: CoroutineScope = rememberCoroutineScope()
+
+    BackHandler(sheetState.isVisible) {
+        coroutineScope.launch { sheetState.hide() }
+    }
+
+    SearchRequestBottomSheet(sheetState, coroutineScope)
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SearchRequestBottomSheet(
+    sheetState: ModalBottomSheetState,
+    coroutineScope: CoroutineScope,
+) {
+    ModalBottomSheetLayout(
+        sheetState = sheetState,
+        sheetContent = { BottomSheet() },
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 72.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom,
+        ) {
+            Button(onClick = {
+                coroutineScope.launch { sheetState.show() }
+            }) {
+                Text(text = "Start Search")
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomSheet() {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.background)
-            .wrapContentSize(Alignment.Center)
+        modifier = Modifier.padding(32.dp).fillMaxSize()
     ) {
         Text(
-            text = "Search",
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colors.onBackground,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 25.sp
+            text = "Bottom sheet",
+            style = MaterialTheme.typography.h6
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            text = "Click outside the bottom sheet to hide it",
+            style = MaterialTheme.typography.body1
         )
     }
 }
